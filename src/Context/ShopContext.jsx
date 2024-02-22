44b474ddc88ea9c8dac599505af2a8a33b8fd042
Antony Murithi <antonymurithi51@gmail.com>
2024-02-22 23:40:55 +0300

    Updates

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext } from "react";
+import all_products from '../../assets/all_products';
+
+export const ShopContext = createContext(null);
+
+const shopContextProvider = (props) => {
+
+    const contextValue = {all_products};
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default shopContextProvider;
